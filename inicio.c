198626e42e932351e87534dd09fbb3925ed2e5f3
Louis Pottier <louispottier44@gmail.com>
2025-06-19 15:53:59 -0300

    Natural language of what we will need to do

--- inicio.c
+++ inicio.c
@@ -8,10 +8,13 @@
 #define NUM_PROCESSOS 4
 #define NUM_LINHAS 101
 #define SIZE_RAM 16
+#define DELTA_T 7 //Number of cycles before a process gets dereferenced and demodified
 
 typedef struct{ //Representa a página
     char referenced; //0 ou 1 se for referenciado recentemente
     char modified; //0 ou 1 se uma linha com
+    char contador_ref; // Para saber há quanto tempo a página nao foi referenciada
+    char contador_mod; // Para saber há quanto tempo a pagina nao foi modificada
     char presenca; //1 se a página presente na RAM, 0 se estiver fora
     long pfnumber; //Em qual endereco da RAM fica essa página
 
@@ -79,9 +82,23 @@ int main(){
     int *ram = (int*)malloc(SIZE_RAM*sizeof(int));
     PageTable *pt = (PageTable*)malloc(NUM_PROCESSOS*sizeof(PageTable));
 
-    for (int i = 0; i < NUM_PROCESSOS; i++){
-        //
-    }
+    // Round-Robin on each file
+    // Example: the first line of the first file is 04 R
+    // We look for the address pt[0].pfnumber
+    // If it is < 0, there is a page fault, the page is outside the RAM.
+        //We have to swap out one page frame and swap in the one we need.
+        // --> use of algorithm NRU and others
+    // Else, it is a hit:
+        // The 'referenced' counter is reset if the line is "R"
+        // The 'modified' counter is reset if the line is "W"
+        // We modify the referenced field if it was 0 to 1 (depends on the DeltaT we chose)
+        // We change the modified field if it was an access to write ("W")
+    // For all the other pages (from all the processes)
+        // Compare the 'ref' and 'mod' counters with delta_T
+        // If ref_counter greater than delta_T, decrement ref_counter
+        // If mod_counter greater than delta_T, decrement mod_counter
+    // Increment the number of cycles
+    // Come back until there is no more access lines in the processes
 
     return 0;
 }
